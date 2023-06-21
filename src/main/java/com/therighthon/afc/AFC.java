a69a978d314f4be0f9fad8cc3b661feef2fa8218
Therighthon <jackwitecki@gmail.com>
2023-06-21 16:56:10 -0700

    Fix server crash, update version number

--- src/main/java/com/therighthon/afc/AFC.java
+++ src/main/java/com/therighthon/afc/AFC.java
@@ -8,12 +8,14 @@ import com.therighthon.afc.common.items.AFCItems;
 import com.therighthon.afc.event.ModEvents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(AFC.MOD_ID)
@@ -34,17 +36,22 @@ public class AFC
         AFCBlocks.register(eventBus);
         AFCEntities.ENTITIES.register(eventBus);
         AFCFeatures.FEATURES.register(eventBus);
-
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::clientSetup);
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::registerColorHandlerBlocks);
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::registerColorHandlerItems);
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::registerClientReloadListeners);
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onEntityRenderers);
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onLayers);
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onBlockColors);
+        if (FMLEnvironment.dist == Dist.CLIENT)
+        {
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::clientSetup);
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::registerClientReloadListeners);
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onEntityRenderers);
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::registerColorHandlerBlocks);
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::registerColorHandlerItems);
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onLayers);
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onBlockColors);
 //        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onItemColors);
-        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onTextureStitch);
+            eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onTextureStitch);
 //        eventBus.addListener(com.therighthon.afc.event.ModEventClientBusEvents::onParticlesRegister);
+        }
+
+
+
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
