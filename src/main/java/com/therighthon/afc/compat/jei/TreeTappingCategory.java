b1b1e60293d38b11b4f9ac904e148050c1b2f1cd
Redeix <brayden.j.m.ford@gmail.com>
2026-07-23 15:11:25 -0500

    better temp string

--- src/main/java/com/therighthon/afc/compat/jei/TreeTappingCategory.java
+++ src/main/java/com/therighthon/afc/compat/jei/TreeTappingCategory.java
@@ -4,6 +4,7 @@ import java.util.List;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import com.therighthon.afc.common.blocks.AFCBlocks;
+import com.therighthon.afc.common.blocks.TapBlock;
 import com.therighthon.afc.common.recipe.TreeTapRecipe;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
@@ -19,6 +20,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
@@ -32,9 +34,12 @@ import org.jetbrains.annotations.NotNull;
 public class TreeTappingCategory extends BaseRecipeCategory<TreeTapRecipe>
 {
 
+    private static final int WIDTH = 180;
+    private static final int HEIGHT = 60;
+
     public TreeTappingCategory(RecipeType<TreeTapRecipe> type, IGuiHelper helper)
     {
-        super(type, helper, helper.createBlankDrawable(118, 50), new ItemStack(AFCBlocks.TREE_TAP.get()));
+        super(type, helper, helper.createBlankDrawable(WIDTH, HEIGHT), new ItemStack(AFCBlocks.TREE_TAP.get()));
 
     }
 
@@ -53,8 +58,7 @@ public class TreeTappingCategory extends BaseRecipeCategory<TreeTapRecipe>
                 public @NotNull List<Component> getTooltip(@NotNull ItemStack ingredient, @NotNull TooltipFlag tooltipFlag) {
                     return ingredient.isEmpty() ? List.of() : ingredient.getTooltipLines(Minecraft.getInstance().player, tooltipFlag);
                 }
-            })
-            .setBackground(slot, -1, -1);
+            });
 
         builder.addSlot(RecipeIngredientRole.OUTPUT, 76, 5)
             .addFluidStack(recipe.getOutput().getFluid(), 1000)
@@ -65,17 +69,20 @@ public class TreeTappingCategory extends BaseRecipeCategory<TreeTapRecipe>
     public void draw(TreeTapRecipe recipe, @NotNull IRecipeSlotsView recipeSlots, @NotNull GuiGraphics graphics, double mouseX, double mouseY)
     {
         arrow.draw(graphics, 48, 3);
-
         var font = Minecraft.getInstance().font;
 
-        Component tempRange = Component.translatable("tooltip.afc.tap.temp_range")
-            .withStyle(ChatFormatting.WHITE)
+        // Temperature Range.
+        Component rangeComponent = Component.empty()
             .append(Component.literal(String.format("%.1f°C", recipe.getMinTemp())).withStyle(ChatFormatting.AQUA))
             .append(Component.literal(" - ").withStyle(ChatFormatting.WHITE))
             .append(Component.literal(String.format("%.1f°C", recipe.getMaxTemp())).withStyle(ChatFormatting.GOLD));
 
+        Component tempRange = Component.translatable("tfc.tooltip.climate_current_temp", rangeComponent)
+            .withStyle(ChatFormatting.WHITE);
+
         graphics.drawString(font, tempRange, 6, 28, 0xFF404040, true);
 
+        // Season Condition.
         if (recipe.springOnly())
         {
             Component springOnly = Component.translatable("tfc.tooltip.calendar_season",
@@ -84,23 +91,53 @@ public class TreeTappingCategory extends BaseRecipeCategory<TreeTapRecipe>
             graphics.drawString(font, springOnly, 6, 40, 0xFF404040, true);
         }
 
+        // 3D Render.
         recipeSlots.getSlotViews().get(0).getDisplayedIngredient(VanillaTypes.ITEM_STACK).ifPresent(stack -> {
             if (stack.getItem() instanceof BlockItem blockItem)
             {
-                BlockState state = blockItem.getBlock().defaultBlockState();
-                PoseStack poseStack = graphics.pose();
-                poseStack.pushPose();
-                poseStack.translate(15, 15, 100);
-                poseStack.mulPose(Axis.XP.rotationDegrees(-15.5f));
-                poseStack.mulPose(Axis.YP.rotationDegrees(22.5f));
-                int scale = 16;
-                poseStack.scale(scale, -scale, scale);
-                poseStack.translate(-0.5, -0.5, -0.5);
-
-                Minecraft.getInstance().getBlockRenderer().renderSingleBlock(state, poseStack, graphics.bufferSource(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, ModelData.EMPTY, RenderType.solid());
-
-                poseStack.popPose();
+                renderTreeTapping(graphics, blockItem.getBlock().defaultBlockState());
             }
         });
+    }
+
+    /**
+     * Render log blocks in a tower of three with a tap in the middle.
+     */
+    private void renderTreeTapping(GuiGraphics graphics, BlockState logState)
+    {
+        PoseStack poseStack = graphics.pose();
+        poseStack.pushPose();
+        poseStack.translate(15, 25, 100);
+        poseStack.mulPose(Axis.XP.rotationDegrees(-15.5f));
+        poseStack.mulPose(Axis.YP.rotationDegrees(22.5f));
+        int scale = 16;
+        poseStack.scale(scale, -scale, scale);
+
+        // Logs.
+        poseStack.pushPose();
+        poseStack.translate(-0.5, -0.5, -0.5);
+        renderBlock(graphics, poseStack, logState, RenderType.solid());
+        poseStack.translate(0, 1, 0);
+        renderBlock(graphics, poseStack, logState, RenderType.solid());
+        poseStack.translate(0, -2, 0);
+        renderBlock(graphics, poseStack, logState, RenderType.solid());
+        poseStack.popPose();
+
+        // Tap.
+        BlockState tapState = AFCBlocks.TREE_TAP.get().defaultBlockState().setValue(TapBlock.FACING, Direction.SOUTH);
+        poseStack.pushPose();
+        poseStack.translate(-0.5, -0.5, 0.5);
+        renderBlock(graphics, poseStack, tapState, RenderType.cutout());
+        poseStack.popPose();
+
+        poseStack.popPose();
+    }
+
+    /**
+     * Helper method to render a blockstate display.
+     */
+    private void renderBlock(GuiGraphics graphics, PoseStack poseStack, BlockState state, RenderType type)
+    {
+        Minecraft.getInstance().getBlockRenderer().renderSingleBlock(state, poseStack, graphics.bufferSource(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, ModelData.EMPTY, type);
     }
 }
